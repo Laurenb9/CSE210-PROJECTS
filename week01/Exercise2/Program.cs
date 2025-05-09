54915d973cb44146dcaf1f874405eaf162a9482d
Laurenb9 <lrendonbuitrago@byupathway.edu>
2025-05-09 14:10:51 -0500

    weekly activities completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+       Console.Write("What is your grade percentage? ");
+       string answer = Console.ReadLine();
+       int percent = int.Parse(answer);
+
+         if (percent >= 90)
+         {
+              Console.WriteLine("You got an A.");
+         }
+         else if (percent >= 80)
+         {
+              Console.WriteLine("You got a B.");
+         }
+         else if (percent >= 70)
+         {
+              Console.WriteLine("You got a C.");
+         }
+         else if (percent >= 60)
+         {
+              Console.WriteLine("You got a D.");
+         }
+         else
+         {
+              Console.WriteLine("You got an F.");
+         }
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
     }
 }
