218b5c940b57493c9854729af21bbbd00120b81b
Laurenb9 <laurendon906@gmail.com>
2025-06-16 18:21:48 -0500

    cycling, running and swimming classes added

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+abstract class Activity
 {
-    static void Main(string[] args)
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); // en km
+    public abstract double GetSpeed();    // en kph
+    public abstract double GetPace();     // min por km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min): Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
     }
 }
---
+++ week07/ExerciseTracking/clases.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main()
+    {
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("04 Nov 2022", 45, 20.0),
+            new Swimming("05 Nov 2022", 30, 40)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
